2b9072f370969aeba3a1f9d17ba72051f83a6ffb
kier <guchengkai066@163.com>
2021-09-08 09:37:30 +0800

    完整功能提交

---
+++ middlewares/authheader.js
@@ -0,0 +1,19 @@
+ module.exports  = (ctx, opts) => {
+
+	 console.log(ctx.header)
+   if (!ctx.header || !ctx.header.MY_TOKEN) {
+          return;
+      }
+      const parts = ctx.header.MY_TOKEN.split(' ');
+	  console.log(parts)
+      if (parts.length === 2) {
+          const scheme = parts[0];
+          const credentials = parts[1];
+          // if (/^Bearer$/i.test(scheme)) {
+          //     return credentials;
+          // }
+      }
+      if (!opts.passthrough) {
+          ctx.throw(401, 'Bad Authorization header format. Format is "Authorization: Bearer <token>"');
+      }
+}
---
+++ middlewares/errorhandle.js
@@ -0,0 +1,10 @@
+ module.exports  = (ctx, next) => {
+  return next().catch((err) => {
+    if (err.status === 401) {
+      ctx.status = 401;
+      ctx.body = 'Protected resource, use Authorization header to get access\n';
+    } else {
+      throw err;
+    }
+  });
+}
